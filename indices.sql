ed94570898b59f945eab5a97f0f2d4073fcf823a
beatriz0freitas <a106853@alunos.uminho.pt>
2025-05-31 19:24:20 +0100

    indexacao

---
+++ indices.sql
@@ -0,0 +1,24 @@
+-- =============================================
+-- Indices
+-- Caso de Estudo: AgroAuto
+-- =============================================
+
+-- Índices para junções na tabela Aluguer
+CREATE INDEX idx_aluguer_idCliente ON Aluguer(idCliente);
+CREATE INDEX idx_aluguer_idFuncionario ON Aluguer(idFuncionario);
+CREATE INDEX idx_aluguer_idTrator ON Aluguer(idTrator);
+-- Índice para consultar por data de inicio e estado de pagamento nos alugueres
+CREATE INDEX idx_aluguer_data_estado ON Aluguer(dataInicio, estadoPagamento);
+
+-- Índice para consultar tratores por preço
+CREATE INDEX idx_trator_precoDiario ON Trator(precoDiario);
+-- Índice para consultar tratores por marca
+CREATE INDEX idx_trator_marca ON Trator(marca);
+-- Índice para verificar tratores livres para aluguer
+CREATE INDEX idx_trator_estado ON Trator(estado);
+-- Índice para junção na tabela Trator
+CREATE INDEX idx_trator_idStand ON Trator(idStand);
+
+
+-- Índice para junção na tabela Funcionário
+CREATE INDEX idx_funcionario_idStand ON Funcionario(idStand);
--- queries.sql
+++ queries.sql
@@ -3,20 +3,15 @@
 -- Caso de Estudo: AgroAuto
 -- =============================================
 
--- =============================================
 -- RM02
--- =============================================
 SELECT * 
 FROM TratoresAlugadosPorStandDiario 
 ORDER BY idStand, marca;
 
 
--- =============================================
 -- RM04
--- =============================================
 DROP PROCEDURE IF EXISTS VerificaClienteAptoAluguer;
 DELIMITER $$
-
 CREATE PROCEDURE VerificaClienteAptoAluguer (
     IN pidClientePretendido INT,
     IN pdataTerminoPretendida DATE
@@ -30,19 +25,14 @@ BEGIN
     FROM AgroAuto.Cliente
     WHERE idCliente = pidClientePretendido;
 END $$
-
 DELIMITER ;
 
 CALL VerificaClienteAptoAluguer(1, '2035-08-01');
 
 
-
--- =============================================
 -- RM10
--- =============================================
 DROP PROCEDURE IF EXISTS FuncionariosComMaisAlugueresMes;
 DELIMITER $$
-
 CREATE PROCEDURE FuncionariosComMaisAlugueresMes (
     IN pDataInicioMes DATE,
     IN pDataFimMes DATE
@@ -69,18 +59,14 @@ BEGIN
         ) AS subquery
     );
 END $$
-
 DELIMITER ;
 
 CALL FuncionariosComMaisAlugueresMes('2025-05-01', '2025-05-31');
 
 
--- =============================================
 -- RM11
--- =============================================
 DROP PROCEDURE IF EXISTS TotalAlugueresPorTrimestre;
 DELIMITER $$
-
 CREATE PROCEDURE TotalAlugueresPorTrimestre(
     IN pdataInicioTrimestre DATE,
     IN pdataFimTrimestre DATE
@@ -90,15 +76,12 @@ BEGIN
     FROM AgroAuto.Aluguer 
     WHERE dataInicio BETWEEN pdataInicioTrimestre AND pdataFimTrimestre;
 END $$
-
 DELIMITER ;
 
 CALL TotalAlugueresPorTrimestre('2025-04-01', '2025-06-30');
 
 
--- =============================================
 -- RM12
--- =============================================
 SELECT * 
 FROM TotalFaturadoPorStandMensal 
 WHERE ano = 2025 AND mes = 5;
