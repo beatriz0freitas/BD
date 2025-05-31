097f7919ca4f09c8c8ff6474d14ce7ebeef08dc5
beatriz0freitas <a106853@alunos.uminho.pt>
2025-05-31 20:09:05 +0100

    funcao custo

---
+++ funcoes.sql
@@ -0,0 +1,24 @@
+-- =============================================
+-- Funções
+-- Caso de Estudo: AgroAuto
+-- =============================================
+
+DROP FUNCTION IF EXISTS calcularCustoAluguer;
+DELIMITER $$
+
+CREATE FUNCTION calcularCustoAluguer(pIdAluguer INT) RETURNS DECIMAL(8,2)
+DETERMINISTIC
+
+BEGIN
+    DECLARE precoTotal DECIMAL(8,2);
+
+    SELECT (DATEDIFF(dataTermino, dataInicio) + 1) * precoDiario
+    INTO precoTotal
+    FROM Aluguer 
+    JOIN Trator ON Aluguer.idTrator = Trator.idTrator
+    WHERE Aluguer.idAluguer = pIdAluguer;
+
+    RETURN precoTotal;
+END$$
+
+DELIMITER ;
